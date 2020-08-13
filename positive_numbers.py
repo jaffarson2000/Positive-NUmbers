3dfd9a0cee4be4116e7ca82cc76ddfc406beaaa6
jaffarson2000 <68461174+jaffarson2000@users.noreply.github.com>
2020-08-13 16:53:33 +0530

    Create positive_numbers.py
    A Python Program to print all positive numbers in range

---
+++ positive_numbers.py
@@ -0,0 +1,14 @@
+#First One
+num=[12,-7,5,64,-14]
+print("Output: ",end="")
+for i in num:
+    if i>=0:
+         print(i,end=",")
+         
+#Second One
+num=[12,14,-95,3]
+print("Output: ",end="[")
+for i in num:
+    if i>=0:
+        print(i,end=",")
+print(end="]")
